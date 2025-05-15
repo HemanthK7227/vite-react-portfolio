53c239dc3d1e349df46656ffdfe1f1ef02073da0
HemanthK7227 <itshemanthkothapalli@gmail.com>
2025-05-15 13:15:58 -0400

    Updated About section and Navbar

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -53,7 +53,7 @@ const AboutSection: React.FC = () => {
             </div>
 
             <a
-              href="https://drive.google.com/uc?export=download&id=1j03PIhnNS3F1n-dK95JQMlajqrJIBQ5F"
+              href="https://drive.google.com/file/d/1Gx1H5yGWEWmMtZTjuCJPUn9GXwXBFiNS/view?usp=sharing"
               className="mt-8 inline-block bg-indigo-600 text-white px-6 py-2 rounded-lg shadow hover:bg-indigo-700 transition"
             >
               Download Resume
